87b7af0b10f3a6a821d1da98d706ecb8e3c4e8ff
xiaoming <xiaoming>
2024-02-16 00:38:49 +0800

    update

---
+++ java-projects/compose-demo/settings.gradle.kts
@@ -0,0 +1,14 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/public")
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    plugins {
+        kotlin("jvm").version(extra["kotlin.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+    }
+}
+
+rootProject.name = "compose-demo"
