f9d257b8e7d9010d107a8c306e51efa98d081efe
liudi <liu_di0101@wuxiapptec.com>
2024-09-18 10:06:57 +0800

    update

--- web-projects/core-antd/components/Layout.tsx
+++ web-projects/core-antd/components/Layout.tsx
@@ -1,7 +1,26 @@
 import { ProLayout, ProLayoutProps } from "@ant-design/pro-components";
+import { createGlobalStyle } from "@liudi0105/core-react";
 
 export type LayoutProps = {} & ProLayoutProps;
 
+const SLayout = createGlobalStyle`
+  html, body, #root {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  pre {
+    font-family: 'Cascadia Mono', 'Consolas', 'Courier New', Courier, monospace;
+    margin: 0;
+  }
+`;
+
 export const Layout = (props: LayoutProps) => {
-  return <ProLayout {...props}></ProLayout>;
+  return (
+    <SLayout>
+      <ProLayout {...props}></ProLayout>
+    </SLayout>
+  );
 };
