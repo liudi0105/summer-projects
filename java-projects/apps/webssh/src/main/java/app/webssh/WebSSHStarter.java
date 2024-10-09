a4f89ee5de3f67ca9b803d4b146300d7f45d3492
liudi <liudi0105@outlook.com>
2024-10-09 18:19:51 +0800

    update

---
+++ java-projects/apps/webssh/src/main/java/app/webssh/WebSSHStarter.java
@@ -0,0 +1,11 @@
+package app.webssh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSSHStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(WebSSHStarter.class);
+    }
+}
